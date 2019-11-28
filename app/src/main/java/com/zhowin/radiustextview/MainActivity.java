3e0d4a6c0212e80a4ba682526b7e022543d6cd4c
Chvigteny <1021237228@qq.com>
2019-11-28 20:20:43 +0800

    去掉了无用的资源

--- app/src/main/java/com/zhowin/radiustextview/MainActivity.java
+++ app/src/main/java/com/zhowin/radiustextview/MainActivity.java
@@ -3,13 +3,16 @@ package com.zhowin.radiustextview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhowin.library.radius.RadiusTextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    RadiusTextView radiusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radiusTextView = findViewById(R.id.radiusTextView);
     }
 }
